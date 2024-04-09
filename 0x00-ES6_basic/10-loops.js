b98a47842a906d5ca5745856666d5c7b397ee155
stephen_adah <mailstephenreal@gmail.com>
2024-04-09 21:35:31 +0100

    11-main.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [...array];
 
-  for (const key in array) {
-    if (Object.prototype.hasOwnProperty.call(newArray, key)) {
-      newArray[key] = appendString + newArray[key];
+  for (let value of newArray) {
+    if (value) {
+      value = appendString + value;
     }
   }
 
