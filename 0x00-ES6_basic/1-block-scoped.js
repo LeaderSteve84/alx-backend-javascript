93fc1103d597e5d9a4c38073f9b925838de19af1
stephen_adah <mailstephenreal@gmail.com>
2024-04-09 08:54:21 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  task = false;
+  var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    task2 = false;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
