f5499bec949f754f1dffbacc701909465c915287
stephen_adah <mailstephenreal@gmail.com>
2024-04-14 13:30:17 +0100

    2-main.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,12 @@
 function handleResponseFromAPI(promise) {
   // append handler using the then method
-  promise.then(
-    // resolve handler
-    () => {
-      console.log('Got a response from the API'); // log message for resolution
-      return { status: 200, body: 'success' }; // return object for resolution
-    },
-    // reject handler
-    () => {
-      console.log('Got a response from the API'); // log message for rejection
-      return new Error(); // return empty object for rejection
-    },
-  );
+  promise.then(() => {
+    console.log('Got a response from the API'); // log message for resolution
+    return { status: 200, body: 'success' }; // return object for resolution
+  }).catch((error) => {
+    console.error(error); // log message for rejection
+    return new Error(); // return empty object for rejection
+  });
 }
 
 export default handleResponseFromAPI;
