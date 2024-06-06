a51b0f31d0b90478eefc6fae9a4685e64b45533f
stephen_adah <mailstephenreal@gmail.com>
2024-06-06 12:38:43 +0100

    correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const inputName = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${inputName}`);
+  if (inputName) process.stdout.write(`Your name is: ${inputName}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
