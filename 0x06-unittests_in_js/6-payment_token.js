660b8757adf5cddfaa21b0b5f701528552fba3f9
stephen_adah <mailstephenreal@gmail.com>
2024-06-09 08:56:30 +0100

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,13 @@
+// 6-paymenttoken.js
+
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({data: 'Successful response from the API' });
+    } else {
+      resolve(undefined);
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
